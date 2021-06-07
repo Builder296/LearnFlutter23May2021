76b3fbaea56215ab9b99522b6a8bc22afe3e17da
builder296 <kessirin296@gmail.com>
2021-06-07 18:50:47 +0700

    Refactor test getImagePathLanguage with group function

--- test/screens/login/login_controller_test.dart
+++ test/screens/login/login_controller_test.dart
@@ -4,15 +4,16 @@ import 'package:example_flutter1/screens/login/login_controller.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 main() {
-  test('should be return flag th when call getImagePathLanguage with Language.th', () {
+  group("getImagePathLanguage", () {
     var controller = LoginController();
-    var actual = controller.getImagePathLanguage(Language.th);
-    expect(actual, "assets/thai-flag.png");
-  });
+    test('should be return flag th when call getImagePathLanguage with Language.th', () {
+      var actual = controller.getImagePathLanguage(Language.th);
+      expect(actual, "assets/thai-flag.png");
+    });
 
-  test('should be return flag en when call getImagePathLanguage with Language.en', () {
-    var controller = LoginController();
-    var actual = controller.getImagePathLanguage(Language.en);
-    expect(actual, "assets/eng-flag.png");
+    test('should be return flag en when call getImagePathLanguage with Language.en', () {
+      var actual = controller.getImagePathLanguage(Language.en);
+      expect(actual, "assets/eng-flag.png");
+    });
   });
 }
